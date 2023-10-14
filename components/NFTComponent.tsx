e92b66c166d4c1c741f32d39164a9b6544300d41
Nyk <0xnykcd@googlemail.com>
2023-10-14 23:05:23 +0200

    nftitems

--- components/NFTComponent.tsx
+++ components/NFTComponent.tsx
@@ -3,72 +3,113 @@ import { useWallet } from '@solana/wallet-adapter-react';
 import React, { useState, useEffect } from 'react'
 import MyMultiButton from './MyMultiButton';
 import NFT from '../types/NFTs';
-import NFTItem from './NFTItems';
 import NFTItems from './NFTItems';
 
 
 function NFTComponent() {
-    const wallet = useWallet();
-    const [nfts, setNfts] = useState<NFT[]>([]); // State to hold NFTs
-    const [loading, setLoading] = useState(false); // State to hold loading status
+	const wallet = useWallet();
+	const [nfts, setNfts] = useState<NFT[]>([]); // State to hold NFTs
+	const [loading, setLoading] = useState(false); // State to hold loading status
+	const [selectedNfts, setSelectedNfts] = useState<NFT[]>([]); // State to hold selected NFTs
+	const [isCheckboxChecked, setIsCheckboxChecked] = useState(false); // State to hold checkbox status
+
+	useEffect(() => {
+		if (wallet.connected) {
+			setLoading(true);
+			const url = process.env.NEXT_PUBLIC_HELIUS_URL || 'default_url';
+			const body = {
+				jsonrpc: '2.0',
+				id: 'my-id',
+				method: 'getAssetsByOwner',
+				params: {
+					ownerAddress: wallet.publicKey?.toBase58(),
+					page: 1,
+					limit: 1000
+				},
+			};
+
+			fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(body),
+			})
+				.then(response => response.json())
+				.then(data => {
+					setNfts(data.result.items);
+					setLoading(false);
+				})
+				.catch((error) => {
+					console.error('Error:', error);
+					setLoading(false);
+				});
+		}
+	}, [wallet.connected]);
+
+	// console.log(nfts)
 
 
-    useEffect(() => {
-        if (wallet.connected) {
-            setLoading(true);
-            const url = process.env.NEXT_PUBLIC_HELIUS_URL || 'default_url';
-            const body = {
-                jsonrpc: '2.0',
-                id: 'my-id',
-                method: 'getAssetsByOwner',
-                params: {
-                    ownerAddress: wallet.publicKey?.toBase58(),
-                    page: 1,
-                    limit: 1000
-                },
-            };
+	const handleSelectNft = (nft: NFT, isSelected: boolean) => {
+		if (isSelected) {
+			setSelectedNfts(prev => [...prev, nft]);
+		} else {
+			setSelectedNfts(prev => prev.filter(item => item.content.metadata.name !== nft.content.metadata.name));
+		}
+	};
 
-            fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(body),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    setNfts(data.result.items);
-                    setLoading(false);
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                    setLoading(false);
-                });
-        }
-    }, [wallet.connected]);
+	console.log(selectedNfts)
 
-    console.log(nfts)
-    return (
-        <>
-            <div className='w-full flex flex-col items-center justify-center gap-4 container mx-auto mt-40 px-4'>
-                {wallet.publicKey ? <>
-                    {
-                        loading ? (
-                            <CircularProgress /> // Show progress bar when loading
-                        ) : (
-                            <div className="w-full">
-                                <h2>Your NFTs</h2>
-                                <div className="w-full">
-                                    <NFTItems nfts={nfts} />
-                                </div>
-                            </div>
-                        )}
-                </>
-                    :
-                    <MyMultiButton />}
-            </div>
-        </>
-    )
+
+	return (
+		<>
+			<div className='w-full flex flex-col items-center justify-center gap-4 container mx-auto mt-40 px-4'>
+				{wallet.publicKey ? <>
+					{
+						loading ? (
+							<CircularProgress /> // Show progress bar when loading
+						) : (
+							<div className="w-full">
+								<h2>Your NFTs</h2>
+								{selectedNfts.length > 0 && <>
+									<div className="w-full max-w-md mx-auto flex items-center justify-center p-4 px-8 rounded-lg bg-red-500 -bg-opacity-80 backdrop-blur-xl fixed top-20 z-50 left-1/2 -translate-x-1/2">
+										<span>Selected NFTs are marked to get burned!</span>
+									</div>
+								</>}
+								<div className="w-full">
+									<NFTItems nfts={nfts} handleSelectNft={handleSelectNft} selectedNfts={selectedNfts} />
+								</div>
+								{selectedNfts.length > 0 && <>
+									<div className="w-full max-w-md flex flex-col items-center justify-center p-4 px-8 rounded-lg bg-black bg-opacity-80  border border-undust-green border-opacity-20 backdrop-blur-xl fixed bottom-8 z-50 left-1/2 -translate-x-1/2">
+										<div className="w-full flex flex-col items-center justify-start gap-2">
+											<span className="text-red-500 text-xl">You are about to burn {selectedNfts.length} NFTs</span>
+											<div className="flex flex-row items-center justify-center gap-2">
+												<input
+													type="checkbox"
+													name="confirm"
+													id=""
+													onChange={(e) => setIsCheckboxChecked(e.target.checked)} // Update checkbox status when it changes
+												/>
+												<span className="text-white text-[10px]">I understand that this action is irreversible</span>
+											</div>
+										</div>
+										<button
+											onClick={() => { }}
+											className="tooltip myFreshButton text-sm break-keep font-bold  flex items-center justify-center  text-white p-4 rounded-[120px] mt-8 w-full disabled:opacity-50 disabled:cursor-not-allowed"
+											disabled={!isCheckboxChecked} // Disable button when checkbox is not checked
+										>
+											Burn selected ({selectedNfts.length}) NFTs
+										</button>
+									</div>
+								</>}
+							</div>
+						)}
+				</>
+					:
+					<MyMultiButton />}
+			</div>
+		</>
+	)
 }
 
 export default NFTComponent
--- components/NFTItems.tsx
+++ components/NFTItems.tsx
@@ -1,22 +1,37 @@
 import React from 'react'
 import NFT from '../types/NFTs'
 
-function NFTItems({ nfts }: any) {
+function NFTItems({ nfts, handleSelectNft, selectedNfts }: any) {
     return (
         <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
-            {nfts.map((nft: NFT) => (
-                <div key={nft.content.metadata.name} className="flex flex-col items-center justify-center w-full h-full border border-undust-green border-opacity-20 rounded-lg">
-                    <div className="flex flex-col items-start justify-between w-full p-4 gap-8">
-                        <div className="w-full h-full object-cover">
-                            <img src={nft.content.links.image} alt={nft.content.metadata.name} className='w-full h-[250px] object-cover' />
-                        </div>
-                        <div className="flex flex-col justify-start items-start text-left w-full gap-2">
-                            <span className="text-xl text-white truncate w-[99%]">{nft.content.metadata.name}</span>
-                            <span className="text-sm text-undust-green"></span>
+            {nfts.map((nft: NFT) => {
+
+                const isSelected = selectedNfts.some((selectedNft: { id: string; }) => selectedNft.id === nft.id);
+
+                return (
+                    <div
+                        onClick={() => handleSelectNft(nft, !isSelected)}
+                        key={nft.id}
+                        className={`relative flex flex-col items-center justify-center w-full h-full border border-undust-green border-opacity-20 rounded-lg ${isSelected ? '!text-red-500' : 'text-white'}`}
+                    >
+                        {isSelected && <div className="absolute h-[70vh] top-8 right-8 z-20">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-12">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                            </svg>
+                        </div>}
+                        <div className="flex flex-col items-start justify-between w-full p-4 gap-8">
+                            <div className="w-full h-full object-cover">
+                                <img src={nft.content.links.image} alt={nft.content.metadata.name} className='w-full h-[250px] object-cover' />
+                            </div>
+                            <div className="flex flex-col justify-start items-start text-left w-full gap-2 font-bold">
+                                <span className={`text-xl truncate w-[99%] ${isSelected ? 'text-red-500' : 'text-white'}`}>{nft.content.metadata.name}</span>
+                                <span className={`text-sm ${isSelected ? 'text-undust-green' : 'text-white'}`}></span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                )
+            })}
         </div>
     )
 }
