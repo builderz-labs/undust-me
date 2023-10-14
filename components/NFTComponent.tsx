10251e08380ebe95e205900399f6c7a87c68e910
Nyk <0xnykcd@googlemail.com>
2023-10-14 22:28:00 +0200

    nftsitems

--- components/NFTComponent.tsx
+++ components/NFTComponent.tsx
@@ -3,6 +3,8 @@ import { useWallet } from '@solana/wallet-adapter-react';
 import React, { useState, useEffect } from 'react'
 import MyMultiButton from './MyMultiButton';
 import NFT from '../types/NFTs';
+import NFTItem from './NFTItems';
+import NFTItems from './NFTItems';
 
 
 function NFTComponent() {
@@ -57,21 +59,7 @@ function NFTComponent() {
                             <div className="w-full">
                                 <h2>Your NFTs</h2>
                                 <div className="w-full">
-                                    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
-                                        {nfts.map((nft) => (
-                                            <div key={nft.content.metadata.name} className="flex flex-col items-center justify-center w-full h-full border border-undust-green border-opacity-20 rounded-lg">
-                                                <div className="flex flex-col items-start justify-between w-full p-4 gap-8">
-                                                    <div className="w-full h-full object-cover">
-                                                        <img src={nft.content.links.image} alt={nft.content.metadata.name} className='w-full h-[250px] object-cover' />
-                                                    </div>
-                                                    <div className="flex flex-col justify-start items-start text-left w-full gap-2">
-                                                        <span className="text-xl text-white truncate w-[99%]">{nft.content.metadata.name}</span>
-                                                        <span className="text-sm text-undust-green"></span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
-                                    </div>
+                                    <NFTItems nfts={nfts} />
                                 </div>
                             </div>
                         )}
--- components/NFTItems.tsx
+++ components/NFTItems.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NFT from '../types/NFTs'
 
-function NFTItem({ nfts }: any) {
+function NFTItems({ nfts }: any) {
     return (
         <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
             {nfts.map((nft: NFT) => (
@@ -21,4 +21,4 @@ function NFTItem({ nfts }: any) {
     )
 }
 
-export default NFTItem
+export default NFTItems
