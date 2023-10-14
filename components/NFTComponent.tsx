ab46a74f89b0a7d58cd87045a5b363dbe6552bfa
Nyk <0xnykcd@googlemail.com>
2023-10-14 22:27:01 +0200

    nfts

--- components/NFTComponent.tsx
+++ components/NFTComponent.tsx
@@ -1,57 +1,85 @@
 import { CircularProgress } from '@mui/material';
 import { useWallet } from '@solana/wallet-adapter-react';
 import React, { useState, useEffect } from 'react'
+import MyMultiButton from './MyMultiButton';
+import NFT from '../types/NFTs';
 
-interface NFT {
-    id: string;
-    image: string;
-    name: string;
-    collection: string;
-    // Add other properties as needed
-}
 
 function NFTComponent() {
     const wallet = useWallet();
     const [nfts, setNfts] = useState<NFT[]>([]); // State to hold NFTs
     const [loading, setLoading] = useState(false); // State to hold loading status
 
+
     useEffect(() => {
-        setLoading(true); // Set loading to true when starting fetch
-        fetchNFTs();
-    }, []);
+        if (wallet.connected) {
+            setLoading(true);
+            const url = process.env.NEXT_PUBLIC_HELIUS_URL || 'default_url';
+            const body = {
+                jsonrpc: '2.0',
+                id: 'my-id',
+                method: 'getAssetsByOwner',
+                params: {
+                    ownerAddress: wallet.publicKey?.toBase58(),
+                    page: 1,
+                    limit: 1000
+                },
+            };
 
-    const fetchNFTs = async () => {
-        // Fetch NFTs from Solana
-        const res = await fetch('https://api.devnet.solana.com/'); // Replace with your API endpoint
-        const data = await res.json();
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(body),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    setNfts(data.result.items);
+                    setLoading(false);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                    setLoading(false);
+                });
+        }
+    }, [wallet.connected]);
 
-
-        setNfts(data);
-        setLoading(false); // Set loading to false when fetch is complete
-    }
-
+    console.log(nfts)
     return (
-        <div className='w-full flex flex-col items-center justify-center gap-4 container mx-auto mt-40 px-4'>
-            {loading ? (
-                <CircularProgress /> // Show progress bar when loading
-            ) : (
-                <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-                    {nfts.map((nft) => (
-                        <div key={nft.id} className="flex flex-col items-center justify-center w-full h-full border border-undust-green border-opacity-20 rounded-lg">
-                            <div className="flex flex-row items-start justify-between w-full p-4 gap-8">
-                                <div className="w-1/2 h-full">
-                                    <img src={nft.image} alt={nft.name} className='w-full h-full object-cover' />
-                                </div>
-                                <div className="flex flex-col justify-start items-start w-full">
-                                    <span className="text-2xl text-white">{nft.name}</span>
-                                    <span className="text-sm text-undust-green">{nft.collection}</span>
+        <>
+            <div className='w-full flex flex-col items-center justify-center gap-4 container mx-auto mt-40 px-4'>
+                {wallet.publicKey ? <>
+                    {
+                        loading ? (
+                            <CircularProgress /> // Show progress bar when loading
+                        ) : (
+                            <div className="w-full">
+                                <h2>Your NFTs</h2>
+                                <div className="w-full">
+                                    <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-4">
+                                        {nfts.map((nft) => (
+                                            <div key={nft.content.metadata.name} className="flex flex-col items-center justify-center w-full h-full border border-undust-green border-opacity-20 rounded-lg">
+                                                <div className="flex flex-col items-start justify-between w-full p-4 gap-8">
+                                                    <div className="w-full h-full object-cover">
+                                                        <img src={nft.content.links.image} alt={nft.content.metadata.name} className='w-full h-[250px] object-cover' />
+                                                    </div>
+                                                    <div className="flex flex-col justify-start items-start text-left w-full gap-2">
+                                                        <span className="text-xl text-white truncate w-[99%]">{nft.content.metadata.name}</span>
+                                                        <span className="text-sm text-undust-green"></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
+                        )}
+                </>
+                    :
+                    <MyMultiButton />}
+            </div>
+        </>
     )
 }
 
