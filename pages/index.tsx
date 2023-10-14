77c9f7e6373fc1b778bbff1092ab9492975709b3
Nyk <0xnykcd@googlemail.com>
2023-10-14 13:09:20 +0200

    link

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
       <Head>
         <title>UnDust.me - Your Easy To Use Solana Wallet Cleaner</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta property="og:image" content="/earnsol.png" />
+        <meta property="og:image" content="https://cdn.discordapp.com/attachments/1158367321644081153/1162708672225480714/telegram-cloud-photo-size-2-5280632887542140878-y.jpg?ex=653cebd5&is=652a76d5&hm=0f68f364c6147541f8319fdc8e9430e46dfd0627b50567da329130f668818fe2&" />
         <meta property="og:title" content="UnDust.me - Your Easy To Use Solana Wallet Cleaner" />
         <meta property="og:description" content="The easiest way to tidy up your wallet & earn SOL" />
         <meta property="og:url" content="https://undust.me" />
