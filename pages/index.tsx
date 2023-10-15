507c2115c008b0a3d72dbe23f0c0fa6437586a62
Nyk <0xnykcd@googlemail.com>
2023-10-15 15:16:32 +0200

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Undust.me</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://undust.me/favicon.ico" type="image/x-icon" />
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
