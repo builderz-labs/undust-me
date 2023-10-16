c383cad63f59ffd6695f8cff35cf7d87df417c65
Nyk <0xnykcd@googlemail.com>
2023-10-16 03:10:49 +0200

    step2 inti

--- components/Stepper/Stepper2.tsx
+++ components/Stepper/Stepper2.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NFTComponent from '../NFTComponent'
 
 function Stepper2() {
     return (
-        <div>Stepper2</div>
+        <div>
+            <NFTComponent />
+        </div>
     )
 }
 
